9321e9fcea9f8a336096066fab9e4bbb30455319
BagoesPantera <bagoespantera01@gmail.com>
2022-10-02 11:50:57 +0800

    create and update

--- src/main/java/com/sakunoki/TodoController.java
+++ src/main/java/com/sakunoki/TodoController.java
@@ -1,11 +1,12 @@
 package com.sakunoki;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class TodoController {
@@ -19,8 +20,32 @@ public class TodoController {
     }
 
     @GetMapping("/todo/{id}")
-    public Todo findOne(@PathVariable Integer id){
-        return service.findOne(id);
+    public ResponseEntity<Todo> findOne(@PathVariable Integer id){
+        try{
+            Todo todo = service.findOne(id);
+            return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+        }catch (NoSuchElementException e){
+            return new ResponseEntity<Todo>(HttpStatus.NOT_FOUND);
+        }
     }
 
+    @PostMapping("/todo")
+    public String create(@RequestBody Todo todo) {
+        service.create(todo);
+        return "Data added successfully";
+    }
+
+    @PutMapping("/todo/{id}")
+    public ResponseEntity<?> update(@RequestBody Todo todo, @PathVariable Integer id){
+        try {
+            Todo todoExist = service.findOne(id);
+            todoExist.setTitle(todo.getTitle());
+            todoExist.setDescription(todo.getDescription());
+            service.create(todoExist);
+
+            return new ResponseEntity<>("Data updated successfully",HttpStatus.OK);
+        }catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
